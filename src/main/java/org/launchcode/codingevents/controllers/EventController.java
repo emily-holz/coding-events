c3554da194eb66dea45cb15d8c29d8ea690527ad
Emily Holzman <emily.r.holzman@gmail.com>
2022-08-28 14:25:52 -0400

    finished first video - created Event class

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
     private static List<Event> events = new ArrayList<>();
 
-
     @GetMapping
-    public String displayAllEvents(Model model){
-        HashMap<String,String> events = new HashMap<>();
+    public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
